adc7782370eece2c850f122639721b6486f39389
Ayush Singh <aayushsingh.code@gmail.com>
2022-10-14 19:58:38 +0530

    staff feature added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from os import read
-from pickle import FALSE
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
-def addnewitem():
-    print("Add item working")
+def addnewitem():    
+    print("add new item working")
+def additemstatus(): 
+    print("add item status working")
 
 def searchitem():
     item_name = input("Enter name of item: ")
@@ -28,56 +30,94 @@ def removeitem():
     print(idf)
 
 def showitem():
-    print("show item working")
+    idf = pd.read_csv('./data/item-detail.csv')
+    print(idf)
+
 
 def addstaff():
     print("add staff working")
 
 def searchstaff():
-    print("search staff working")
+    staffid = int(input('Enter staff id: '))
+    isdf = pd.read_csv('./data/staff.csv')
+    df = isdf.loc[isdf["staff_id"] == staffid]
+    if df.empty:
+        print("No staff found with given id")
+    else:
+        print(df)
+
 
 def removestaff():
-    print("remove staff working")
+    staffid = int(input("Enter staff id: "))
+    isdf = pd.read_csv('./data/staff.csv')
+    isdf = isdf.drop(isdf[isdf["staff_id"] == staffid].index)
+    isdf.to_csv('./data/staff.csv', index = False)
+    print("Staff removed successfully")
+    print(isdf)
+
 
 def showdetail():
-    print("show detail working")
+    isdf = pd.read_csv('./data/staff.csv')
+    print(isdf)
 
 def chart():
-    print("chart working")
+    print("Press 1 - Item and their cost")
+    print("Press 2 - Staff and delivery")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        idf = pd.read_csv('./data/item-detail.csv')
+        idf = idf[["item_name", "item_cost"]]
+        idf.plot("item_name", "item_cost", kind='bar')
+        plt.xlabel('Item Name ------->')
+        plt.ylabel('Item Cost ------->')
+        plt.show()
+    elif choice == 2:
+        idf = pd.read_csv('./data/staff.csv')
+        idf = idf[["staff_id", "staff_score"]]
+        idf.plot("staff_id","staff_score", kind='bar')
+        plt.xlabel('Staff ID ----->')
+        plt.ylabel('Staff Score ----->')
+        plt.show()
+
+    else:
+        print("Invalid choice")
 
 
 
 def showmenu():
     print("Press 1 - Add new item")
-    print("Press 2 - Search for an item")
-    print("Press 3 - Remove an item")
-    print("Press 4 - Show all item")
-    print("Press 5 - Add new staff")
-    print("Press 6 - Search for staff")
-    print("Press 7 - Remove staff")
-    print("Press 8 - Show details of staff")
-    print("Press 9 - To view chart")
-    print("Press 10 - To exit")
+    print("Press 2 - Add item status")
+    print("Press 3 - Search for an item")
+    print("Press 4 - Remove an item")
+    print("Press 5 - Show all item")
+    print("Press 6 - Add new staff")
+    print("Press 7 - Search for staff")
+    print("Press 8 - Remove staff")
+    print("Press 9 - Show details of staff")
+    print("Press 10 - To view chart")
+    print("Press 11 - To exit")
     choice = int(input("Enter your choice:  "))
     if choice == 1:
         addnewitem()
     elif choice == 2:
-        searchitem()
+        additemstatus()    
     elif choice == 3:
-        removeitem()
+        searchitem()
     elif choice == 4:
-        showitem()
+        removeitem()
     elif choice == 5:
-        addstaff()
+        showitem()
     elif choice == 6:
-        searchstaff()
+        addstaff()
     elif choice == 7:
-        removestaff()
+        searchstaff()
     elif choice == 8:
-        showdetail()
+        removestaff()
     elif choice == 9:
-        chart()
+        showdetail()
     elif choice == 10:
+        chart()
+    elif choice == 11:
         exit()
 
 
